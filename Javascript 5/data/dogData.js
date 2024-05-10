b4267883809e7018ab7ef098bae055aaa67cfbde
suzuki <keisuke.suzuki@novel.jpn.com>
2024-05-10 14:02:27 +0900

    JavaScript5 完了

---
+++ Javascript 5/data/dogData.js
@@ -0,0 +1,14 @@
+import readlineSync from "readline-sync";
+import Dog from "../class/dog";
+
+const dog1 = new Dog("レオ", 4, "チワワ");
+
+const name = readlineSync.question("名前を入力してください:");
+
+const age = readlineSync.questionInt("年齢を入力してください:");
+
+const breed = readlineSync.question("犬種を入力してください:");
+
+const dog2 = new Dog(name, age, breed);
+
+export {dog1, dog2};
