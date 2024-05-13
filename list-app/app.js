23cb8ca1bc61d174844561b14269b747b1f3b904
suzuki <keisuke.suzuki@novel.jpn.com>
2024-05-13 16:52:24 +0900

    Node,js 3 完了

--- list-app/app.js
+++ list-app/app.js
@@ -16,28 +16,59 @@ host: 'localhost',
 app.get('/', (req, res) => {
     res.render('top.ejs');
   });
-
-app.get('/index', (req, res) => {
+  
+  app.get('/index', (req, res) => {
     connection.query(
-        'SELECT * FROM items',
-        (error, results) => {
-            res.render('index.ejs', {items: results});
-        }
+      'SELECT * FROM items',
+      (error, results) => {
+        res.render('index.ejs', {items: results});
+      }
     );
-});
-
-app.get('/new', (req, res) => {
+  });
+  
+  app.get('/new', (req, res) => {
     res.render('new.ejs');
-});
-
-app.post('/create', (req, res) => {
+  });
+  
+  app.post('/create', (req, res) => {
     connection.query(
-        'INSERT INTO items (name) VALUES (?)',
-        [req.body.itemName],
-        (error, results) => {
-            res.redirect('/index');
-        }
+      'INSERT INTO items (name) VALUES (?)',
+      [req.body.itemName],
+      (error, results) => {
+        res.redirect('/index');
+      }
     );
-});
+  });
+  
+  app.post('/delete/:id', (req, res) => {
+    connection.query(
+      'DELETE FROM items WHERE id = ?',
+      [req.params.id],
+      (error, results) => {
+        res.redirect('/index');
+      }
+    );
+  });
+  
+  app.get('/edit/:id', (req, res) => {
+    connection.query(
+        'SELECT * FROM items WHERE id = ?',
+        [req.params.id],
+        (error, results) => {
+          res.render('edit.ejs', {item: results[0]});
+        }
+      );
+  });
 
-app.listen(3000);
+  app.post('/update/:id', (req, res) => {
+    connection.query(
+        'UPDATE items SET name = ? WHERE id = ?',
+        [req.body.itemName, req.params.id],
+        (error, results) => {
+          res.redirect('/index');
+        }
+      );
+  });
+  
+  app.listen(3000);
+  
